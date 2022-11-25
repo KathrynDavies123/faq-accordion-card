1c42814f49f8376dc79762b4c7cb2f640684e46e
KathrynDavies123 <kathryn.davies1233@gmail.com>
2022-11-25 18:04:13 +0100

    final parts of js and css done for desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 let accordion = document.getElementsByClassName("faqaccordion");
+let arrow = document.getElementsByClassName("arrowbutton");
 
 for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function () {
+        accordion[i].classList.toggle("expanded");
+        arrow[i].classList.toggle("arrowrotate");
         let expansion = this.nextElementSibling;
         if (expansion.style.display === "block") {
             expansion.style.display = "none";
